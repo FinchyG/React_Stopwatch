6030e70aebb2a59d91d25d254decf6dc8610b1b1
FinchyG <lagaunt120@yahoo.com>
2018-03-16 21:36:35 +0000

    working child update parent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Child } from './Child';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Parent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {title: "placeholder title"};
+        this.changeTheWorld = this.changeTheWorld.bind(this);
+    }
+
+    changeTheWorld() {
+        const newTitle = "New Title";
+        this.setState({title: newTitle});
+    }
+
+    render() {
+      return (
+        <div>
+            <Child doWhatever={this.changeTheWorld} title={this.state.title} />
+        </div>
+      );
+    }
+
+}
+
+ReactDOM.render(<Parent />, document.getElementById('root'));
