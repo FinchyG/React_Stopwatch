e985fd241f0bbf360c2db036a82ac7a276a45ca3
FinchyG <lagaunt120@yahoo.com>
2018-03-18 19:45:38 +0000

    changable p content

--- src/Child.js
+++ src/Child.js
@@ -3,7 +3,8 @@ import React from 'react';
 export const Child = (props) => {
     return (
         <div>
-          <button onClick={props.doWhatever}>{props.title}</button>
+          <button onClick={props.doWhatever}>Click Me</button>
+          <p>{props.title}</p>
         </div>
       );
 }
