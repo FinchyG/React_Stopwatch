a4ee6aa2a8defbfda7b08a8ec2a07cdd7aaba0fe
FinchyG <lagaunt120@yahoo.com>
2018-05-06 19:22:39 +0100

    working left (right) button

---
+++ src/Image_paths.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import questionMark from './images/questionMark.png';
+import entertainment from './images/entertainment.png';
+import general_knowledge from './images/general_knowledge.png';
+import geography from './images/geography.png';
+import history from './images/history.png';
+import science_and_nature from './images/science_and_nature.png';
+import sport from './images/sport.png';
+
+export const Image_paths = [
+    questionMark,
+    general_knowledge,
+    geography,
+    history,
+    science_and_nature,
+    sport
+  ]
--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Image_paths} from './Image_paths';;
 
-class Homepage extends React.Component {
+class Carousel extends React.Component {
+    
+    constructor(props){
+
+        super(props);
+
+        this.state = {currentImage: 0};
+
+        this.leftImage = this.leftImage.bind(this);
+
+    }
+
+    leftImage() {
+
+        this.setState({currentImage: this.state.currentImage + 1 });
+
+    }
 
     render() {
-      return (
-        <div>
+
+            
+        return (
+            
             <div>
+                <button onClick={this.leftImage}>Left</button>
+                <img src = {Image_paths[this.state.currentImage]} />
             </div>
-            <div>
-            </div>
-        </div>
-      );
+        )
     }
 
 }
 
-ReactDOM.render(<Homepage />, document.getElementById('root'));
+ReactDOM.render(<Carousel />, document.getElementById('root'));
