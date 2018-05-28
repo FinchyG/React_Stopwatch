f0fe2778fc9b5228147c4cfca90757a43fa26f1d
FinchyG <lagaunt120@yahoo.com>
2018-05-28 12:22:13 +0100

    stateful watch_display

--- src/index.js
+++ src/index.js
@@ -4,14 +4,52 @@ import './index.css';
 
 class Stopwatch extends React.Component {
 
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            centi_seconds: "00",
+            seconds      : "00",
+            minutes      : "00"
+        }
+
+        this.myCounter = this.myCounter.bind(this);
+        
+    }
+
+    myCounter() {
+
+        const centi_seconds = this.state.centi_seconds + 1;
+        const seconds       = this.state.seconds;
+        const minutes       = this.state.minutes;
+
+        this.setState({centi_seconds});
+        
+        /*minutes = parseInt(minutes);
+        seconds = parseInt(seconds);
+
+        seconds = centi_seconds > 99 ? seconds + 1 : seconds;
+        minutes = seconds > 59 ? minutes + 1 : minutes;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        seconds = seconds > 59 ? "0" + 0 : seconds;
+
+        centi_seconds = centi_seconds < 10 ? "0" + centi_seconds : centi_seconds;
+        centi_seconds = centi_seconds > 99 ? "0" + 0 : centi_seconds;
+
+        document.getElementById("watch_display").innerHTML = minutes + ":" + seconds + ":" + centi_seconds;
+        centi_seconds++; */
+
+    }
+
+
 render(){
 
     return (
 
         <div>
             <div id="display_background">
-                <p id="watch_display">00:00:00</p>
+                <p id="watch_display">{this.state.minutes}:{this.state.seconds}:{this.state.centi_seconds}</p>
             </div>
 
             <div id="buttons">
@@ -19,10 +57,6 @@ render(){
                 <button onclick="myTimer = setInterval(myCounter, 10)">Start</button>
 
                 <button onclick="clearInterval(myTimer)">Stop</button>
-
-                <button onclick="reset()">Reset</button>
-
-                <button onclick="split()">Split</button>
 
             </div>
 
